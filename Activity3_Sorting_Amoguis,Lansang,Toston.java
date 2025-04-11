6384e94821115b6db0afa227dd64d1740c0219a2
abbie120805 <abegaillansang60@gmail.com>
2025-04-11 15:04:20 +0800

    Add files via upload

--- Activity3_Sorting_Amoguis,Lansang,Toston.java
+++ Activity3_Sorting_Amoguis,Lansang,Toston.java
@@ -61,16 +61,4 @@ public class StudentSorter {
         }
     }
 
-    public static void bubbleSort(Student[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j].grade > arr[j + 1].grade) {
-                    Student temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
 }
